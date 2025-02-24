2fb8312c2a2107c8ab3ec6d4ef40b4970873a56b
sgoudham <sgoudham@gmail.com>
2025-02-24 01:28:06 +0000

    feat!: overhaul & simplify

--- README.md
+++ README.md
@@ -6,76 +6,60 @@
 </h3>
 
 <p align="center">
-    <a href="https://github.com/catppuccin/hyper-catppuccin/stargazers"><img src="https://img.shields.io/github/stars/catppuccin/hyper-catppuccin?colorA=363a4f&colorB=b7bdf8&style=for-the-badge"></a>
-    <a href="https://github.com/catppuccin/hyper-catppuccin/issues"><img src="https://img.shields.io/github/issues/catppuccin/hyper-catppuccin?colorA=363a4f&colorB=f5a97f&style=for-the-badge"></a>
-    <a href="https://github.com/catppuccin/hyper-catppuccin/contributors"><img src="https://img.shields.io/github/contributors/catppuccin/hyper-catppuccin?colorA=363a4f&colorB=a6da95&style=for-the-badge"></a>
+    <a href="https://github.com/catppuccin/hyper/stargazers"><img src="https://img.shields.io/github/stars/catppuccin/hyper?colorA=363a4f&colorB=b7bdf8&style=for-the-badge"></a>
+    <a href="https://github.com/catppuccin/hyper/issues"><img src="https://img.shields.io/github/issues/catppuccin/hyper?colorA=363a4f&colorB=f5a97f&style=for-the-badge"></a>
+    <a href="https://github.com/catppuccin/hyper/contributors"><img src="https://img.shields.io/github/contributors/catppuccin/hyper?colorA=363a4f&colorB=a6da95&style=for-the-badge"></a>
 </p>
 
+<p align="center">
+  <img src="assets/preview.webp"/>
+</p>
 
-<div align="center">
-  <img src="media/preview.png" width="100%" />
-</div>
+## Previews
 
-## Contents
-
-- [Install](#install)
-- [Usage](#usage)
-- [License](#license)
-
-## Install
-**Please note that the name of NPM package and the theme itself is `hypurr` and not `hyper-catppuccin`.**\
-Add the following line to `plugins` field in your `~/.hyper.js`
-```js
-plugins: [
-  'hypurr#latest'
-]
-```
+<details>
+<summary>🌻 Latte</summary>
+<img src="assets/latte.webp"/>
+</details>
+<details>
+<summary>🪴 Frappé</summary>
+<img src="assets/frappe.webp"/>
+</details>
+<details>
+<summary>🌺 Macchiato</summary>
+<img src="assets/macchiato.webp"/>
+</details>
+<details>
+<summary>🌿 Mocha</summary>
+<img src="assets/mocha.webp"/>
+</details>
 
 ## Usage
-A total of 4 different color palettes are available in catppuccin theme --
-- [Latte](https://github.com/catppuccin/catppuccin#-palette)
-- [Frappé](https://github.com/catppuccin/catppuccin#-palette)
-- [Macchiato](https://github.com/catppuccin/catppuccin#-palette)
-- [Mocha](https://github.com/catppuccin/catppuccin#-palette)
 
-You can use the one you like from the four by adding the name of the theme (make sure you type frappé correctly if you're using it!) to the `catppuccinTheme` field in your `~/.hyper.js` like this --
-```js
-module.exports = {
-  ...
-  config : {
-    ...
-    catppuccinTheme: 'Frappé'
-    ...
-  }
-  ...
-}
-```
-By default, the theme is set to `macchiato`.
+1. Add plugin to your configuration file:
 
-## License
+   ```js
+   module.exports = {
+     plugins: ["@catppuccin/hyper#1.0.8"],
+   };
+   ```
 
-[MIT](license.md) 
+2. Choose your theme:
 
-## FAQ
+   ```js
+   module.exports = {
+     config: {
+       theme: "catppuccin-mocha", // or "catppuccin-latte", "catppuccin-frappe", "catppuccin-macchiato"
+     },
+   };
+   ```
 
-*   Q: ***"Which shell prompt is used the screenshots?"***
+## 💝 Thanks to
 
-    A: [starship](https://github.com/starship/starship), a minimal, blazing-fast, and infinitely customizable prompt for any shell.
-
-*   Q: ***"Which shell prompt is used the screenshots?"***
-
-    A: It’s [macchina](https://github.com/Macchina-CLI/macchina), with some [modifications](https://github.com/Sped0n/macchina/blob/hypurr/hypurr.md).
-
-## Contributors
-
-- Ashish ([@asrvd](https://github.com/asrvd))
-- Adal Zanabria ([@AdalZanabria](https://github.com/AdalZanabria))
-- Nikita Sobolev ([@sobolevn](https://github.com/sobolevn))
-- Spedon ([@Spedon](https://github.com/Sped0n))
+- Hammy ([@sgoudham](https://github.com/sgoudham))
 
 &nbsp;
 
 <p align="center"><img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/footers/gray0_ctp_on_line.svg?sanitize=true" /></p>
 <p align="center">Copyright &copy; 2021-present <a href="https://github.com/catppuccin" target="_blank">Catppuccin Org</a>
 <p align="center"><a href="https://github.com/catppuccin/catppuccin/blob/main/LICENSE"><img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=MIT&logoColor=d9e0ee&colorA=363a4f&colorB=b7bdf8"/></a></p>
-
--- index.js
+++ index.js
@@ -1,155 +1,37 @@
-"use strict";
+const { flavorEntries } = require("@catppuccin/palette");
 
-const palettes = {
-  latte: {
-    backgroundColor: "#eff1f5",
-    foregroundColor: "#4c4f69",
-    selectionColor: "#acb0be",
-    cursorColor: "#dc8a78",
-    cursorAccentColor: "eff1f5",
+const theme = flavorEntries.reduce((acc, [name, palette]) => {
+  acc[`catppuccin-${name}`] = {
+    backgroundColor: palette.colors.base.hex,
+    foregroundColor: palette.colors.text.hex,
+    selectionColor: palette.colors.surface0.hex,
+    cursorColor: palette.colors.rosewater.hex,
+    cursorAccentColor: palette.colors.base.hex,
     colors: {
-      black: "#5c5f77",
-      blue: "#1e66f5",
-      red: "#d20f39",
-      green: "#40a02b",
-      yellow: "#df8e1d",
-      magenta: "#ea76cb",
-      cyan: "#179299",
-      white: "#acb0be",
-      lightBlack: "#6c6f85",
-      lightWhite: "#bcc0cc",
-      lightBlue: "#1e66f5",
-      lightRed: "#d20f39",
-      lightGreen: "#40a02b",
-      lightYellow: "#df8e1d",
-      lightMagenta: "#ea76cb",
-      lightCyan: "#179299",
+      black: palette.ansiColors.black.normal.hex,
+      blue: palette.ansiColors.blue.normal.hex,
+      red: palette.ansiColors.red.normal.hex,
+      green: palette.ansiColors.green.normal.hex,
+      yellow: palette.ansiColors.yellow.normal.hex,
+      magenta: palette.ansiColors.magenta.normal.hex,
+      cyan: palette.ansiColors.cyan.normal.hex,
+      white: palette.ansiColors.white.normal.hex,
+      lightBlack: palette.ansiColors.black.bright.hex,
+      lightBlue: palette.ansiColors.blue.bright.hex,
+      lightRed: palette.ansiColors.red.bright.hex,
+      lightGreen: palette.ansiColors.green.bright.hex,
+      lightYellow: palette.ansiColors.yellow.bright.hex,
+      lightMagenta: palette.ansiColors.magenta.bright.hex,
+      lightCyan: palette.ansiColors.cyan.bright.hex,
+      lightWhite: palette.ansiColors.white.bright.hex,
     },
-  },
-  frappé: {
-    backgroundColor: "#303446",
-    foregroundColor: "#c6d0f5",
-    selectionColor: "#626880",
-    cursorColor: "#f2d5cf",
-    cursorAccentColor: "#232634",
-    colors: {
-      black: "#51576d",
-      blue: "#8caaee",
-      red: "#e78284",
-      green: "#a6d189",
-      yellow: "#e5c890",
-      magenta: "#f4b8e4",
-      cyan: "#81c8be",
-      white: "#b5bfe2",
-      lightBlack: "#626880",
-      lightWhite: "#a5adce",
-      lightBlue: "#8caaee",
-      lightRed: "#e78284",
-      lightGreen: "#a6d189",
-      lightYellow: "#e5c890",
-      lightMagenta: "#f4b8e4",
-      lightCyan: "#81c8be",
-    },
-  },
-  macchiato: {
-    backgroundColor: "#24273a",
-    foregroundColor: "#cad3f5",
-    selectionColor: "#5b6078",
-    cursorColor: "#f4dbd6",
-    cursorAccentColor: "#181926",
-    colors: {
-      black: "#494d64",
-      blue: "#8aadf4",
-      red: "#ed8796",
-      green: "#a6da95",
-      yellow: "#eed49f",
-      magenta: "#f5bde6",
-      cyan: "#8bd5ca8bd5ca",
-      white: "#b8c0e0",
-      lightBlack: "#5b6078",
-      lightWhite: "#a5adcb",
-      lightBlue: "#8aadf4",
-      lightRed: "#ed8796",
-      lightGreen: "#a6da95",
-      lightYellow: "#eed49f",
-      lightMagenta: "#f5bde6",
-      lightCyan: "#8bd5ca",
-    },
-  },
-  mocha: {
-    backgroundColor: "#1e1e2e",
-    foregroundColor: "#cdd6f4",
-    selectionColor: "#585b70",
-    cursorColor: "#f5e0dc",
-    cursorAccentColor: "#11111b",
-    colors: {
-      black: "#45475A",
-      blue: "#89b4fa",
-      red: "#f38ba8",
-      green: "#a6e3a1",
-      yellow: "#f9e2af",
-      magenta: "#f5c2e7",
-      cyan: "#94e2d5",
-      white: "#bac2de",
-      lightBlack: "#585b70",
-      lightWhite: "#a6adc8",
-      lightBlue: "#89b4fa",
-      lightRed: "#f38ba8",
-      lightGreen: "#a6e3a1",
-      lightYellow: "#f9e2af",
-      lightMagenta: "#f5c2e7",
-      lightCyan: "#94e2d5",
-    },
-  },
-};
+  };
+  return acc;
+}, {});
 
 exports.decorateConfig = (config) => {
-  const theme = palettes[config.catppuccinTheme.toLowerCase() || "macchiato"];
+  const chosenTheme = theme[config.theme ?? "catppuccin-mocha"];
   return Object.assign({}, config, {
-    ...theme,
-    termCSS: config.termCSS || "",
-    css: `
-      ${config.css}
-      .hyper_main {
-        border: none !important;
-      }
-      .header_header {
-        color: ${theme.foregroundColor};
-      }
-      .header_windowHeaderWithBorder {
-        border-color: ${theme.backgroundColor} !important;
-      }
-      .tabs_borderShim {
-        border-color: transparent !important;
-      }
-      .tab_tab {
-        border: 0;
-      }
-      .tab_tab.tab_active {
-        background: ${theme.backgroundColor};
-        color: ${theme.foregroundColor};
-        transition: all 140ms ease;
-      }
-      .tab_tab:not(.tab_active) {
-        background: ${theme.colors.black};
-        color: ${theme.colors.lightWhite};
-      }
-      .tab_tab:not(.tab_active):hover {
-        background: ${theme.colors.lightBlack};
-        color: ${theme.foregroundColor};
-        transition: background 140ms ease;
-      }
-      .tab_icon {
-        width: 15px;
-        height: 15px;
-      }
-      .tab_shape {
-        width: 7px;
-        height: 7px;
-      }
-      .splitpane_divider {
-        background-color: ${theme.colors.lightBlack} !important;
-      }
-      `,
+    ...chosenTheme,
   });
 };
